6b8ef9dbe3601a87dbea6a5dd3bd14218e868402
Karoladd <karoline859@hotmail.com>
2022-10-14 15:52:53 -0300

    pages build and deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-/* import { Envelope, Lock } from 'phosphor-react';
+import { Envelope, Lock } from 'phosphor-react';
 import { Button } from './components/Button';
 import { Checkbox } from './components/Checkbox';
 import { Heading } from './components/Heading';
 import { Text } from './components/Text';
 import { TextInput } from './components/TextInput';
-import { Logo } from './Logo'; */
+import { Logo } from './Logo';
 
 import './styles/global.css';
 
 export function App() {
   return (
+    /* 
     <>
-    <h1 className="font-bold text-2xl">Hello Word</h1>
-    <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-black hover:bg-cyan-300">
-      Enviar
-    </button>
+      <h1 className="font-bold text-2xl">Hello Word</h1>
+      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-black hover:bg-cyan-300">
+        Enviar
+      </button>
     </>
-    /* <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
+    
+    */
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
       <header className="flex flex-col items-center">
         <Logo />
 
@@ -68,7 +71,6 @@ export function App() {
           <a href="" className="text-gray-400 underline hover:text-gray-200">Não possui conta? Crie uma agora!</a>
         </Text>
       </footer>
-    </div> */
-    
+    </div>
   )
 }
